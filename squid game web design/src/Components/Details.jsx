ba2b968f737a67effc86451e549c34799aa8b130
Dineshchandra47 <dineshchandraindia5@gmail.com>
2023-08-06 12:12:36 +0530

    Add:Updated Details  Component

--- squid game web design/src/Components/Details.jsx
+++ squid game web design/src/Components/Details.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+import Video from '../assets/VIDEOS.png'
+import Video1 from '../assets/VIDEO1.jpg'
+import Video2 from '../assets/VIDEO2.jpg'
+import {blue, pink, fontLightColor} from '../utlis/colors'
+import {FaPlay} from 'react-icons/fa'
 
 const Details = () => {
-  return <>Details
-  </>
+  return <Section id='details' className='defaultMargin'>Details
+  </Section>
 }
+
+const Section = styled.section`
+  
+`
+
+
 export default Details
