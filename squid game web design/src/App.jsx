a5771a0c1f4f8c3d860f33c9794d4354623d4c6e
Dineshchandra47 <dineshchandraindia5@gmail.com>
2023-08-06 13:16:07 +0530

    Add:Updated Animations

--- squid game web design/src/App.jsx
+++ squid game web design/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {  useEffect} from 'react'
 // import './App.css'
 import Hero from './Components/Hero'
 import Info from './Components/Info'
@@ -6,9 +6,34 @@ import Episodes from './Components/Episodes'
 import Videos from './Components/Videos'
 import Details from './Components/Details'
 import Footer from './Components/Footer'
+import scrollreveal from 'scrollreveal'
 
 const App = () => {
-
+  
+  useEffect(() => {
+    const sr = scrollreveal({
+      origin: "bottom",
+      distance: "80px",
+      duration: 2000,
+      reset: false,
+    });
+    sr.reveal(
+      `
+        #hero,
+        #info,
+        #episodes,
+        #details,
+        #videos,
+        #footer
+    `,
+      {
+        opacity: 0,
+        margin:0,
+        interval: 500,
+      }
+    );
+  }, []);
+  
   return (
     <>
     <Hero/>
